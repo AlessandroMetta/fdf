e1db5973ec6e71c54b266e0aa76752670a1002bb
AlessandroMetta <ametta@e4r4p4.42roma.it>
2021-11-04 12:46:58 +0100

    first push

---
+++ fdf_main.c
@@ -0,0 +1,21 @@
+#include "libft/libft.h"
+#include <stdio.h>
+#include <fcntl.h>
+
+int main(int argc, char **argv)
+{
+	int fd;
+
+	if (argc != 2)
+	{
+		ft_putstr("Wrong number of argument\n");
+		return 1;
+	}
+	fd = open(argv[1], O_RDONLY);
+	if (fd < 0)
+	{
+		ft_putstr("Impossible to open the specific file\n");
+		return 1;
+	}
+
+}
---
+++ libft/libft.h
@@ -0,0 +1,9 @@
+#ifndef __LIBFT_H__
+# define __LIBFT_H__
+
+#include <unistd.h>
+
+int ft_strlen(char* str);
+void ft_putstr(char* str);
+
+#endif
