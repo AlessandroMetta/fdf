06c5a8e18a563553cef36026a61f8c5fd1f5973a
AlessandroMetta <ametta@e3r6p7.42roma.it>
2021-11-08 11:29:57 +0100

    boh

---
+++ libft/ft_putstr.c
@@ -0,0 +1,6 @@
+#include "libft.h"
+
+void ft_putstr(const char *str)
+{
+	write(1, str, ft_strlen(str));
+}
--- srcs/fdf_main.c
+++ srcs/fdf_main.c
@@ -6,18 +6,19 @@ int main(int argc, char **argv)
 {
 	int fd;
 
+	fd = 0;
 	if (argc != 2)
 	{
-		ft_putstr_fd("Wrong number of argument\n", 1);
+		ft_putstr("Wrong number of argument\n");
 		return 1;
 	}
 	fd = open(argv[1], O_RDONLY);
 	if (fd < 0)
 	{
-		ft_putstr_fd("Impossible to open the specific file\n", 1);
+		ft_putstr("Impossible to open the specific file\n");
 		return 1;
 	}
-	ft_putstr_fd("File open\n", 1);
+	ft_putstr("File open\n");
 	close(fd);
 	return (0);
 }
