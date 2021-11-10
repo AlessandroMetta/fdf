85b5b007c0f9f0e83fadf2f2347bc43cc078e3a5
AlessandroMetta <ametta@e3r8p1.42roma.it>
2021-11-10 10:47:06 +0100

    working on get_next_line

--- libft/get_next_line.c
+++ libft/get_next_line.c
@@ -1,78 +1,78 @@
 #include "libft.h"
 
-int	ft_read_line(char **p_el, char **rescue, int fd, int buff_size)
+int free_buffer(char *buffer)
 {
-	char	*line_red;
-	char	*temp;
-	int		return_value;
-
-	return_value = buff_size;
-	while (!(*p_el = ft_strchr(*rescue, '\n')) && return_value == buff_size)
-	{
-		if (!(line_red = (char *)malloc(sizeof(char) * (buff_size + 1))))
-			return (-1);
-		if ((return_value = read(fd, line_red, buff_size)) < 0)
-		{
-			free(line_red);
-			return (-1);
-		}
-		line_red[return_value] = '\0';
-		if (!(temp = ft_strjoin(*rescue, line_red)))
-		{
-			free(line_red);
-			return (-1);
-		}
-		free(*rescue);
-		free(line_red);
-		*rescue = temp;
-	}
-	return (return_value);
+	free(buffer);
+	return (-1);
 }
 
-int	ft_return(char **p_el, char **rescue)
+int ft_return(char **ptr_newline, char *buffer)
 {
-	char *temp;
-
-	if (*p_el)
+	char *new_line;
+	
+	if(*ptr_newline)
 	{
-		if (!(temp = ft_strdup(*p_el + 1)))
-		{
-			free(*rescue);
-			return (-1);
-		}
-		free(*rescue);
-		*rescue = temp;
+		new_line = ft_strdup(*ptr_newline + 1);
+		if (new_line)
+			return (free_buffer(buffer));
+		free(buffer);
+		buffer = new_line;
 		return (1);
 	}
-	free(*rescue);
-	*rescue = NULL;
+	free(buffer);
+	buffer = NULL;
 	return (0);
 }
 
-int	get_next_line(int fd, char **line)
+int read_buffer(char *ptr_newline, char *buffer, int fd)
 {
-	static char		*rescue[256];
-	char			*p_el;
+	int		read_ret;
+	char	*read_buffer;
+	char	*new_line;
+
+	read_ret = BUFFER_SIZE;
+	ptr_newline = ft_strchr(buffer, '\n');
+	while (!ptr_newline && read_ret == BUFFER_SIZE)
+	{
+		read_buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+		if (!read_buffer)
+			return (-1);
+		read_ret = read(fd, read_buffer, BUFFER_SIZE);
+		if (read_ret < 0)
+			return (free_buffer(read_buffer));
+		read_buffer[read_ret] = '\0';
+		new_line = ft_strjoin(buffer, read_buffer);
+		if (!new_line)
+			return (free_buffer(read_buffer));
+		free(buffer);
+		free(read_buffer);
+		buffer = new_line;
+		ptr_newline = ft_strchr(buffer, '\n');
+	}
+	return (read_ret);
+}
+
+int get_next_line(int fd, char **line)
+{
+	static char	*buffer[256];
+	char		*ptr_newline;
 
 	if (fd < 0 || fd > 255 || !line || BUFFER_SIZE <= 0)
 		return (-1);
-	p_el = NULL;
-	if (!(rescue[fd]))
+	ptr_newline = NULL;
+	if (!(buffer[fd]))
 	{
-		if (!(rescue[fd] = ft_strdup("")))
+		buffer[fd] = ft_strdup("");
+		if (!(buffer[fd]))
 			return (-1);
 	}
-	if ((ft_read_line(&p_el, &rescue[fd], fd, BUFFER_SIZE)) < 0)
-	{
-		free(rescue[fd]);
-		return (-1);
-	}
-	if (p_el)
-		*p_el = '\0';
-	if (!(*line = ft_strdup(rescue[fd])))
-	{
-		free(rescue[fd]);
-		return (-1);
-	}
-	return (ft_return(&p_el, &rescue[fd]));
+	if ((read_buffer(ptr_newline, buffer[fd], fd)) < 0)
+		return (free_buffer(buffer[fd]));
+	if (ptr_newline)
+		*ptr_newline = '\0';
+	*line = ft_strdup(buffer[fd]);
+	if (!(*line))
+		return (free_buffer(buffer[fd]));
+	return (ft_return(&ptr_newline, buffer[fd]));
 }
+
