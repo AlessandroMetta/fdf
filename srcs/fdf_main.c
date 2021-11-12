5ffe3e4139976b857a068e2cca28b37d3c64ec7f
AlessandroMetta <ametta@student.42roma.it>
2021-11-12 12:53:10 +0100

    adding the map

--- srcs/fdf_main.c
+++ srcs/fdf_main.c
@@ -6,7 +6,7 @@
 /*   By: ametta <ametta@student.42roma.it>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 12:50:46 by ametta            #+#    #+#             */
-/*   Updated: 2021/11/12 11:36:13 by ametta           ###   ########.fr       */
+/*   Updated: 2021/11/12 12:52:14 by ametta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,25 +32,64 @@ int	open_file(int argc, char **argv)
 	return (fd);
 }
 
+char ***allocate_map(char ***map, int prev_dim)
+{
+	char	***new;
+	int		i;
+
+	i = 0;
+	if (!map)
+	{
+		new = (char ***)malloc(sizeof(char **) * 1);
+		if (!new)
+			return (NULL);
+	}
+	else
+	{
+		new =  (char ***)malloc(sizeof(char **) * (prev_dim + 1));
+		while (i < prev_dim)
+		{
+			new[i] = map[i];
+			i++;
+		}
+	}
+	free(map);
+	return (new);
+}
+/*
+char ***add_row(char ***map, char **split_line, int prev_dim)
+{
+	char	***new;
+	int		line;
+	int		word;
+
+	// alloco il contenitore (formato da varie linee) + 1 (NULL)
+	new = (char ***)malloc(sizeof(char **) * prev_dim + 1);
+	// alloco quante parole contiene ogni linea + 1 (NULL)
+	new[line] = (char **)malloc(sizeof(char *) * num_word + 1);
+	// allocco la parola
+	new[line][word] = ft_strdup(map[line][word]);
+	// alloco la nuova linea e le nuove parole come precedentemnte fatto
+}
+*/
 void	map_create(int fd)
 {
 	char	*line_red;
-	char	**mat;
+	char	**split_ret;
 	int		i;
 
 	i = 0;
 	while (get_next_line(fd, &line_red))
 	{
-		mat = ft_split(line_red, ' ');
-		while (mat[i])
+		split_ret = ft_split(line_red, ' ');
+		while (split_ret[i])
 		{
-			ft_putendl(mat[i]);
-			free(mat[i]);
+			ft_putendl(split_ret[i]);
+			free(split_ret[i]);
 			i++;
 		}
-		free(mat[i]);
-		free(mat);
-		mat = NULL;
+		free(split_ret);
+		split_ret = NULL;
 		ft_putendl("------");
 		free(line_red);
 		i = 0;
