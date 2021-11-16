8f49bf59758673c8261ffd7f79f4e189fcf63445
AlessandroMetta <ametta@student.42roma.it>
2021-11-16 18:11:50 +0100

    lavorando

--- srcs/fdf_main.c
+++ srcs/fdf_main.c
@@ -6,11 +6,20 @@
 /*   By: ametta <ametta@student.42roma.it>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 12:50:46 by ametta            #+#    #+#             */
-/*   Updated: 2021/11/15 15:45:45 by ametta           ###   ########.fr       */
+/*   Updated: 2021/11/16 16:22:16 by ametta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+
+typedef struct	s_data
+{
+	void		*img;
+	char		*addr;
+	int			bits_per_pixel;
+	int			line_length;
+	int			endian;
+}				t_data;
 
 typedef struct s_fdf
 {
@@ -18,7 +27,16 @@ typedef struct s_fdf
 	char	***map;
 	void	*connect_id;
 	void	*window_id;
+	t_data	img;
 }	t_fdf;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
 
 int	open_file(int argc, char **argv)
 {
@@ -38,6 +56,7 @@ int	open_file(int argc, char **argv)
 	}
 	return (fd);
 }
+
 int	exit_fdf(t_fdf *this)
 {
 	free_map(this->map);
@@ -54,6 +73,15 @@ int	key_pressed(int keycode, t_fdf *this)
 	return (0);
 }
 
+void	win_gen(t_fdf *this)
+{
+	this->connect_id = mlx_init();
+	this->window_id = mlx_new_window(this->connect_id, 500, 500, "fdf");
+	this->img.img = mlx_new_image(this->connect_id, 500, 500);
+	this->img.addr = mlx_get_data_addr(this->img.img, &this->img.bits_per_pixel,
+									&this->img.line_length, &this->img.endian);
+}
+
 int	main(int argc, char **argv)
 {
 	t_fdf	this;
@@ -62,10 +90,14 @@ int	main(int argc, char **argv)
 	if (this.fd < 0)
 		return (-1);
 	this.map = map_create(this.fd);
-	this.connect_id = mlx_init();
-	this.window_id = mlx_new_window(this.connect_id, 500, 500, "fdf");
+	map_dim();
+	/*
+	win_gen(&this);
 	mlx_hook(this.window_id, 2, 1L<<0, key_pressed, &this);
 	mlx_hook(this.window_id, 17, 1L<<5, exit_fdf, &this);
+	mlx_put_image_to_window(this.connect_id, this.window_id, this.img.img, 0, 0);
 	mlx_loop(this.connect_id);
+	*/
+	exit_fdf(this);
 	return (0);
 }
